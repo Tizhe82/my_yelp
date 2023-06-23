a5d714c2bc9f99482308ce01f1ba371c2da7fd03
Tizhe82 <tizhepaul@gmail.com>
2023-06-23 10:13:21 -0700

    added authentication and graphql

--- src/graphql/subscriptions.ts
+++ src/graphql/subscriptions.ts
@@ -1,33 +1,40 @@
-// tslint:disable
+/* tslint:disable */
+/* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateRestaurant = `subscription OnCreateRestaurant {
-  onCreateRestaurant {
-    id
-    clientId
-    name
-    description
-    city
+export const onCreateRestaurant = /* GraphQL */ `
+  subscription OnCreateRestaurant {
+    onCreateRestaurant {
+      id
+      name
+      description
+      city
+      createdAt
+      updatedAt
+    }
   }
-}
 `;
-export const onUpdateRestaurant = `subscription OnUpdateRestaurant {
-  onUpdateRestaurant {
-    id
-    clientId
-    name
-    description
-    city
+export const onUpdateRestaurant = /* GraphQL */ `
+  subscription OnUpdateRestaurant {
+    onUpdateRestaurant {
+      id
+      name
+      description
+      city
+      createdAt
+      updatedAt
+    }
   }
-}
 `;
-export const onDeleteRestaurant = `subscription OnDeleteRestaurant {
-  onDeleteRestaurant {
-    id
-    clientId
-    name
-    description
-    city
+export const onDeleteRestaurant = /* GraphQL */ `
+  subscription OnDeleteRestaurant {
+    onDeleteRestaurant {
+      id
+      name
+      description
+      city
+      createdAt
+      updatedAt
+    }
   }
-}
 `;
